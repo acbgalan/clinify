4e9261e710a07b9d111be972df5c5be6a9de521c
Carlos Blanco <acbgalan@gmail.com>
2025-12-15 00:07:38 +0100

    The patient mapping is taken out in a separate class.

---
+++ src/Clinify.Data/Configurations/PatientConfiguration.cs
@@ -0,0 +1,61 @@
+﻿using Clinify.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinify.Data.Configurations
+{
+    public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder.ToTable("Patients");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+            builder.Property(p => p.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+            builder.HasIndex(p => new { p.FirstName, p.LastName });
+
+            builder.Property(p => p.BirthDate)
+            .IsRequired()
+            .HasColumnType("date");
+
+            builder.Property(p => p.Gender)
+            .IsRequired();
+
+            builder.Property(p => p.Phone)
+            .IsRequired()
+            .HasMaxLength(20);
+
+            builder.Property(p => p.Email)
+            .IsRequired()
+            .HasMaxLength(200);
+
+            builder.HasIndex(p => p.Email)
+            .IsUnique();
+
+            builder.Property(p => p.CreatedAt)
+            .IsRequired();
+
+            builder.Property(p => p.UpdatedAt)
+            .IsRequired(false);
+
+            builder.Property(p => p.CreatedBy)
+            .IsRequired();
+
+            builder.Property(p => p.UpdatedBy)
+            .IsRequired(false);
+        }
+    }
+}
--- src/Clinify.Data/Context/ApplicationContext.cs
+++ src/Clinify.Data/Context/ApplicationContext.cs
@@ -1,10 +1,12 @@
-﻿using Clinify.Data.Entities;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Clinify.Data.Configurations;
+using Clinify.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Clinify.Data.Context
 {
@@ -20,52 +22,7 @@ namespace Clinify.Data.Context
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Patient>(entity =>
-            {
-                entity.ToTable("Patients");
-
-                entity.HasKey(p => p.Id);
-
-                entity.Property(p => p.FirstName)
-                .IsRequired()
-                .HasMaxLength(50);
-
-                entity.Property(p => p.LastName)
-                .IsRequired()
-                .HasMaxLength(100);
-
-                entity.HasIndex(p => new { p.FirstName, p.LastName });
-
-                entity.Property(p => p.BirthDate)
-                .IsRequired()
-                .HasColumnType("date");
-
-                entity.Property(p => p.Gender)
-                .IsRequired();
-
-                entity.Property(p => p.Phone)
-                .IsRequired()
-                .HasMaxLength(20);
-
-                entity.Property(p => p.Email)
-                .IsRequired()
-                .HasMaxLength(200);
-
-                entity.HasIndex(p => p.Email)
-                .IsUnique();
-
-                entity.Property(p => p.CreatedAt)
-                .IsRequired();
-
-                entity.Property(p => p.UpdatedAt)
-                .IsRequired(false);
-
-                entity.Property(p => p.CreatedBy)
-                .IsRequired();
-
-                entity.Property(p => p.UpdatedBy)
-                .IsRequired(false);
-            });
+            modelBuilder.ApplyConfiguration(new PatientConfiguration());
         }
 
     }
