fa470532f39f603bce12f285056a390ec2a4348c
Carlos Blanco <acbgalan@gmail.com>
2025-12-15 23:27:38 +0100

    The enum Gender is shared between the entity and the DTO, so it is moved to Shared to avoid a reference from Shared to Data.

--- src/Clinify.Shared/Patient/Input/CreatePatientRequest.cs
+++ src/Clinify.Shared/Patient/Input/CreatePatientRequest.cs
@@ -1,25 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 using Clinify.Shared.Common.Enums;
 
-namespace Clinify.Data.Entities
+namespace Clinify.Shared.Patient.Input
 {
-    public class Patient
+    public class CreatePatientRequest
     {
-        public Guid Id { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public DateOnly BirthDate { get; set; }
         public Gender Gender { get; set; }
         public required string Phone { get; set; }
         public required string Email { get; set; }
-        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
-        public DateTimeOffset? UpdatedAt { get; set; }
-        public Guid CreatedBy { get; set; }
-        public Guid? UpdatedBy { get; set; }
+
     }
 }
