2d150ae6e479f4b35ad3ba6e977c260a7b14b9fa
Carlos Blanco <acbgalan@gmail.com>
2025-12-15 23:30:48 +0100

    Installed NuGet packages for Fluent Validation. Added PatientRepository as service.

--- src/Clinify.Server/Program.cs
+++ src/Clinify.Server/Program.cs
@@ -1,4 +1,5 @@
 using Clinify.Data.Context;
+using Clinify.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationContext")));
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
