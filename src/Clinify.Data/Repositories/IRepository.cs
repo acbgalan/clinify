7eef749604b653740244e59d243329aabeae742a
Carlos Blanco <acbgalan@gmail.com>
2025-12-15 01:26:51 +0100

    Core repositories interfaces

---
+++ src/Clinify.Data/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinify.Data.Repositories
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        T? Get(Guid id);
+        List<T> GetAll();
+        void Update(T entity);
+        void Delete(Guid id);
+        void Delete(T entity);
+        bool Exits(Guid id);
+        int Save();
+
+    }
+}
---
+++ src/Clinify.Data/Repositories/IRepositoryAsync.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinify.Data.Repositories
+{
+    public interface IRepositoryAsync<T>
+    {
+        Task AddAsync(T entity);
+        Task<T?> GetAsync(Guid id);
+        Task<List<T>> GetAllAsync();
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(Guid id);
+        Task DeleteAsync(T entity);
+        Task<bool> ExistsAsync(Guid id);
+        Task<int> SaveAsync();
+    }
+}
