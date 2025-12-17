2f9337033c29364ea463c359479617b3197bc367
Carlos Blanco <acbgalan@gmail.com>
2025-12-18 00:14:01 +0100

    Type overloading with ServiceResult and ServiceResult<T>. Some service methods do not return data, for example, deleting a record.

---
+++ src/Clinify.Shared/Common/ServiceResultGeneric.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinify.Shared.Common
+{
+    public class ServiceResult<T> : ServiceResult
+    {
+        public T? Data { get; set; }
+    }
+}
