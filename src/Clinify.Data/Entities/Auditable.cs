15f196afd7fee14d6d412f47ccbfe2f506bcaf5a
Carlos Blanco <acbgalan@gmail.com>
2025-12-20 02:41:07 +0100

    Misc

---
+++ src/Clinify.Data/Entities/Auditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinify.Data.Entities
+{
+    internal class Auditable
+    {
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset? UpdatedAt { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+    }
+}
--- src/Clinify.Data/Entities/Doctor.cs
+++ src/Clinify.Data/Entities/Doctor.cs
@@ -12,12 +12,14 @@ namespace Clinify.Data.Entities
         public Guid Id { get; set; } = Uuid7.NewUuid7();
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
-        public int Specialty { get; set; }
+        public int SpecialtyId { get; set; }
         public string ResumeExtract { get; set; } = string.Empty;
         public string ResumeExperience { get; set; } = string.Empty;
         public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
         public DateTimeOffset? UpdatedAt { get; set; }
         public Guid CreatedBy { get; set; }
         public Guid? UpdatedBy { get; set; }
+
+        public required Specialty Specialty { get; set; }
     }
 }
